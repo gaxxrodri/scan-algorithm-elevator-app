236f4a492531d3445a3e4dc7d0b93366e5e916ed
GaxRodri <gaspar.unlp@gmail.com>
2023-05-23 21:11:30 +1200

    Add stop logic and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import { floors } from './constants'
 import './App.css'
 import { useElevator } from './hooks/useElevator'
+import { ElevatorStatus } from './types.d'
 
 const App = () => {
-  const { callElevator, currentFloor, currentStatus } = useElevator()
+  const { callElevator, currentFloor, currentStatus, queue } = useElevator()
 
   return (
     <>
       <div>Elevator app </div>
       <div className='floating'>
         <h4>Current Floor {currentFloor}</h4>
-        <h4>Current status {currentStatus}</h4>
+        <div style={{ color: currentStatus === ElevatorStatus.Running ? 'green' : 'red' }}>
+          Current status {currentStatus}
+        </div>
       </div>
       <div className='elevator-container'>
         {floors.map((floor: number) => {
           return (
             <div key={floor} className='floor-container'>
               <button
+                style={{
+                  border: queue.some(
+                    request => request.floor === floor && request.isGoingUp && request.dropUser !== true
+                  )
+                    ? '1px solid green'
+                    : '',
+                }}
                 className='up-button'
                 onClick={() => {
                   callElevator(floor, true)
@@ -25,9 +35,23 @@ const App = () => {
                 Up
               </button>
               <div className='floor-text-container' style={{ borderColor: currentFloor === floor ? '#646cff' : '' }}>
-                <h5 className='floor-text-number'>{floor}</h5>
+                <h5
+                  className='floor-text-number'
+                  style={{
+                    color: queue.some(request => request.floor === floor && request.dropUser === true) ? 'violet' : '',
+                  }}
+                >
+                  {floor}
+                </h5>
               </div>
               <button
+                style={{
+                  border: queue.some(
+                    request => request.floor === floor && !request.isGoingUp && request.dropUser !== true
+                  )
+                    ? '1px solid red'
+                    : '',
+                }}
                 className='down-button'
                 onClick={() => {
                   callElevator(floor, false)
